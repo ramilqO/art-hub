ae1ca64b5152b3cae9b0f4cfffe39b2364a5e1c7
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-10-03 00:25:23 +0600

    Deploy errors fix 2

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,37 +1,11 @@
-'use client'
-
-import PageLayout from '@/components/globals/PageLayout/PageLayout'
-import { GetStaticProps, NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import Post from '@/components/globals/Post/Post'
-import axios from 'axios'
+import { NextPage } from "next";
 
 const Page: NextPage = () => {
-    // const [articles, setArticles] = useState([])
-    // const getPosts = async (url: string) => {
-    //     const articles = await (await axios(url)).data
-    //     setArticles(articles)
-    // }
+  return (
+    <div>
+      <h1>Next page</h1>
+    </div>
+  );
+};
 
-    // useEffect(() => {
-    //     getPosts('/api/feed')
-    // }, [])
-
-    return (
-        <PageLayout showSideComments showSidebar>
-            {/* {articles.map((post: any) => (
-                <Post
-                    key={post.title}
-                    id={post.id}
-                    title={post.title}
-                    content={post.content}
-                    author={post.author}
-                    createdAt={post.createdAt}
-                />
-            ))} */}
-            <h1>Hello</h1>
-        </PageLayout>
-    )
-}
-
-export default Page
+export default Page;
